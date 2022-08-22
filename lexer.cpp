5d33bc6913fb25360c51e211d8e3cf62b4cc5bd6
Ishtiaque Zahid <ruko97@gmail.com>
2022-08-22 10:09:49 +0600

    written a getNextUnicode() function to help scanning

--- lexer.cpp
+++ lexer.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cctype>
 #include <vector>
+#include <cassert>
 
 enum Token {
 	tok_eof = -1,
@@ -20,4 +21,45 @@ enum Token {
 	tok_number = -9
 };
 
-int 
+// Returns a 4-byte integer representing the unicode value of the next character
+int getNextUnicode() {
+	int output = 0;
+	int thisChar = getchar();
+	
+	if (thisChar == EOF) return thisChar;
+
+	if (thisChar >> 7 == 0) return thisChar;
+
+	int remaining_bytes = 0;
+	if (thisChar >> 5 == 0x06) {
+		output = thisChar & 0x1F;
+		remaining_bytes = 1;
+	}
+	if (thisChar >> 4 == 0x0E) {
+		output = thisChar & 0x0F;
+		remaining_bytes = 2;
+	}
+	if (thisChar >> 3 == 0x1E) {
+		output = thisChar & 0x07;
+		remaining_bytes = 3;
+	}
+
+	for (int i = 1; i <= remaining_bytes; i++) {
+		output <<= 6;
+		thisChar = getchar();
+		assert(thisChar != EOF);
+		output += thisChar & 0x3F;
+	}
+
+	return output;
+}
+
+int main() {
+	int output;
+	while (true) {
+		output = getNextUnicode();
+		if (output == EOF) break;
+		printf("0x%X\n", output);
+	}
+	return 0;
+}
