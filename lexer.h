b9000d91e3e35302991533b8b6aeedc083784dec
Ishtiaque Zahid <ruko97@gmail.com>
2022-08-22 12:12:42 +0600

    move definitions to lexer.h

---
+++ lexer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+enum Token {
+	tok_eof = -1,
+
+	tok_move = -2,
+	tok_turn_left = -3,
+
+	tok_if = -4,
+	tok_else = -5,
+
+	tok_while = -6,
+
+	tok_not = -7,
+
+	tok_front_blocked = -8,
+
+	tok_number = -9
+};
+
+static int getNextUnicode();
+int getTok();
