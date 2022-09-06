aadf5758b1a7009463109dd963a74de21a9404d2
Ishtiaque Zahid <ruko97@gmail.com>
2022-09-06 15:33:30 +0600

    added code to read instructions and add them in vector called 'instructions'

--- main.cpp
+++ main.cpp
@@ -12,9 +12,27 @@ int main(int argc, char **argv) {
 
     auto program = ParseProgram();
 
-    std::ofstream ofs(argc >= 2 ? argv[1] : "temp.krl", std::ios::out);
+    const char *filename = argc >= 2 ? argv[1] : "temp.krl";
+
+    std::ofstream ofs(filename, std::ios::out);
+    if (!ofs)
+        std::cerr << "Couldn't create temporary file" << std::endl;
 
     program->codegen(ofs);
+
+    std::string instruction;
+    std::vector<std::string> instructions;
+
+    std::ifstream ifs(filename, std::ios::in);
+    if (!ifs)
+        std::cerr << "File named " << filename << "not found after adding "
+                  << "the genrated program to it" << std::endl;
+
+    while (!ifs.eof()) {
+        std::getline(ifs, instruction);
+        if (!(ifs.eof() && instruction.length() == 0))
+            instructions.push_back(instruction);
+    }
 
     return 0;
 }
